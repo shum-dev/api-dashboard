249db160842f858159a418cbe42b79d207175cad
Shum <89135509130@mail.ru>
2023-01-25 14:54:15 +0300

    add PORT to env

--- dashboard-server/index.js
+++ dashboard-server/index.js
@@ -7,7 +7,7 @@ const express = require('express'),
       authRoutes = require('./routes/auth'),
       request = require('request');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
